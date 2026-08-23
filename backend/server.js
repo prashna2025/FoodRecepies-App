b4fbb04164155aa0eec27b79f55a018be9756df4
Prashna Raut <prashnaraut32@gmail.com>
2026-08-23 16:00:19 +0545

    changes are done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express=require("express")
 const app = express()
+const dotenv=require("dotenv").config()
 
 const PORT=process.env.PORT || 3000
 
+app.get("/",(req,res)=>{
+    res.json({message:"hello"})
+})
 app.listen(PORT,(err)=> {
     console.log(`app is listening on port ${PORT}`)
 })
